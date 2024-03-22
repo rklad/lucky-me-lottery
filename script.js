fa21a1a5e6aa988b525b8a7b4058abbf6c9314e8
rklad <525rkladerman@frhsd.com>
2024-03-22 14:57:15 -0400

    started js got and displayed name in list

---
+++ script.js
@@ -0,0 +1,33 @@
+const nameArray = [] // CREATE A LIST TO STORE NAMES
+
+
+
+function addName() {
+    const nameInput = document.getElementById('nameInput')
+    const name = nameInput.value.trim() //GET A TRIMMED VERSION OF THE NAME FROM THE INPUT BOX
+    nameArray.push(name)
+    displayNames()
+    nameInput.value = ''
+}
+
+
+function displayNames(){
+    const nameList = document.getElementById('nameList') //GET UL ELEMENT
+    nameList.innerHTML = '' //CLEARS THE LIST 
+
+    for (let i = 0; i < nameArray.length; i++) { //0 IS THE FIRST TIME THROUGH THE LOOP
+        const name = nameArray[i] //GET THE CURRENT NAME FROM THE ARRAY
+        
+        const li = document.createElement('li')
+        li.className = 'list-group-item'
+        const span = document.createElement('span')
+        span.textContent = name 
+        
+        li.appendChild(span) //PUTS SPAN TAG INSIDE OF LI 
+
+        nameList.appendChild(li)
+    }
+
+    } 
+
+document.getElementById('addNameBtn').addEventListener('click', addName)
